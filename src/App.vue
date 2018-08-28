e89e01ac31780a49642b22bc8e4fb0c0da4e164b
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-08-28 13:22:17 +0200

    component without data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
+//Start import <some_name> from <path_of_the_file>
+import ProductListOne from './components/ProductListOne';
+import ProductListTwo from './components/ProductListTwo';
+//End import <some_name> from <path_of_the_file>
 
+export default {
+  // Start register the components
+  components:{
+    // Start ==> variable : value
+    //the tag name : the component which you import
+    'product-list-one':ProductListOne,
+    'product-list-two':ProductListTwo
+    // End ==> variable : value
+  },
+  // End register the components
+
+  // Start name of the component App
+  name: 'app',
+  // End name of the component App
+
+  // Start data of App
+  data () {
+  // Start return object
+    return {
+      // Start ==> variable : value
+      products:[
+        {name: 'Banana Skin', price: 20},
+        {name: 'Shiny Star', price: 40},
+        {name: 'Green Shells', price: 60},
+        {name: 'Red Shells', price: 80}
+      ]
+      // End ==> variable : value
     }
+  // End return object
   }
+  // End data of App
+
 }
 </script>
 
---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="product-list-two">
+    <h2>Product List two</h2>
+    <ul>
+      <li v-for=""></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
