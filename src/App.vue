7dc33517bacce7f249c7a285783df35003d9283f
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-08-28 13:35:06 +0200

    you send data but the other side not excepect the data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
-    <product-list-one></product-list-one>
+    <!--
+      the right hand side contains the data in the components
+      the left hand side contains the vairable in the components which in the tag name
+      v-bind:products = "products"
+      v-bind:products  means you will have variable products
+      or data with in variable products in the component product_list_one
+      the values of it is  "products"
+      which value comes from the current component App
+      this is the way to pass the data from current component to another
+      NOTE:
+      v-bind:x="y"
+      send the variable x with the value of y
+      important
+      in the component product_list_one the data not available since
+      the component to listen to vuejs and not excepected to get data with this name products
+      this is the use of props
+      we must put props: ['products'] to allow it to get the data
+      -->
+    <product-list-one v-bind:products="products"></product-list-one>
     <product-list-two></product-list-two>
   </div>
 </template>
