bce154d8f76794b61d4b019556d772d8ef665a4e
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-08-28 13:40:08 +0200

    allow to get the data which sent from App

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,14 +2,17 @@
   <div id="product-list-one">
     <h2>Product List One</h2>
     <ul>
-      <li v-for=""></li>
+      <li v-for="product in products" :key="product">
+          <span class="name">{{product.name}}</span>
+          <span class="price">${{product.price}}</span>
+        </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+props:['products'],
   data () {
     return {
 
